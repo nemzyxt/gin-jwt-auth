dcc636546f43b25851b475afd2130efe21eea694
Nemuel Wainaina <bR0S3@protonmail.com>
2023-08-31 07:08:14 +0300

    Create unprotected route

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	router := gin.Default()
+
+	router.GET("/unprotected", unprotectedRouteHandler);
+	router.GET("/protected", protectedRouteHandler);
+
+	router.Run(":1234");
+	fmt.Println("Server up and running ...");
+}
+
+func unprotectedRouteHandler(c *gin.Context) {
+	c.Writer.WriteString("This is the unprotected route");
+}
+
+func protectedRouteHandler(c *gin.Context) {
+
 }
